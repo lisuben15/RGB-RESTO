b0743978a15e3461dfdfd002b94433a7a03247bf
Ariel Damián Rodríguez <ariel.fonavi17@gmail.com>
2023-10-28 12:06:44 -0300

    Se inicio el proceso de conexion a base de datos. y creamos algunas clases mas.

---
+++ Manager/AccesoDatos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Manager
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=DESKTOP-2360JBV\\\\SQLEXPRESS1; database=BD_Resto; integrated security=true\");");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+            conexion.Open();
+            lector = comando.ExecuteReader();
+            }
+
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (conexion != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
---
+++ Manager/ServicioMarcaBebidas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+
+namespace Manager
+{
+    public class ServicioMarcaBebidas
+    {
+        public List<MarcaBebidas> listarMB()
+        {
+
+			List<MarcaBebidas> lista = new List<MarcaBebidas> ();
+			AccesoDatos datos = new AccesoDatos();
+
+			try
+			{
+				datos.setearConsulta("select IdMarca, NombreMarca from MarcaBebidas ");
+				datos.ejecutarLectura();
+
+				while (datos.Lector.Read())
+				{
+					MarcaBebidas aux = new MarcaBebidas();
+					aux.IdMarca = (int)datos.Lector["IdMarca"];
+					aux.NombreMarca = (string)datos.Lector["NombreMarca"];
+
+					lista.Add(aux);
+				}
+				return lista;
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+			finally
+			{
+				datos.cerrarConexion();
+			}
+        }
+
+    }
+}
