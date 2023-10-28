ac9d961c2bf208bef2225d2858f2c178c3fb2698
Ariel Damián Rodríguez <ariel.fonavi17@gmail.com>
2023-10-28 19:46:33 -0300

    se agregaron algunas clases, debemos seguir armando el dominio...

---
+++ Dominio/Mesa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Mesa
+    {
+        public int NumeroMesa {  get; set; }
+        public bool Estado {  get; set; }
+        public int IdMesero { get; set; }
+        
+    }
+}
+
---
+++ Dominio/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pedido
+    {
+       public int NumeroPedido { get; set; }
+       public int NumeroMesa {  get; set; }  
+       public string Entrada { get; set; }
+       public string Plato { get; set; }
+        public string Bebida { get; set;}
+        public string Postre { get; set;}
+    }
+}
