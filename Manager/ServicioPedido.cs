bb72d8c3686cc04f886687ec638a6679b6db6837
lisuben15 <garcia_gaston_1991@hotmail.com>
2023-11-02 09:32:50 -0300

    se agrego unas referencias en ServicioPedidos

--- Manager/ServicioPedido.cs
+++ Manager/ServicioPedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Manager
 {
-    internal class ServicioPedido
+    public class ServicioPedido
     {
+
+        // HACER EL  ABML DE PEDIDOS
+
+
+
+        //public List<Pedido> PedidosPorMesa(int idMesa)
+        //{
+        //    // pedidos que se generaroon
+        //    return;
+        //}
+
+        //public List<Pedido> PedidosPorMesero(string meseroNombre)
+        //{
+        //    return;
+        //}
+
+
+        // funcion que devulva los pedidos segun id usuario ( id mesero ) traera todos los pedidos del id mesero
+
+        // funcion devolver pedido segun el id mesa
+
+
+
     }
 }
