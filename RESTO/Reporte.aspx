7beb140951515fea1dd833895298a1b6710b7cec
Ariel Damián Rodríguez <ariel.fonavi17@gmail.com>
2023-11-19 11:51:10 -0300

    Le logró que la ddl Pueda mostrar el id de usuario de los meseros, y que al seleccionar, la dgv muestre el reporte de ventas y el saldo total. Seguimos trabajando!

--- RESTO/Reporte.aspx
+++ RESTO/Reporte.aspx
@@ -6,14 +6,15 @@
     <h1>Reporte</h1>
 
     <asp:DropDownList ID="ddlReporte" runat="server" OnSelectedIndexChanged="ddlReporte_SelectedIndexChanged" AutoPostBack="true" style="margin-bottom: 20px;"></asp:DropDownList>
+    <br />
 
-    <asp:GridView ID="dgvReporte" runat="server" AutoGenerateColumns="false" DataKeyNames="IdMenu" class="table table-dark table-striped" style="width: 250%;">
+    <asp:GridView ID="dgvReporte" runat="server" AutoGenerateColumns="false" DataKeyNames="IdMenu" class="table table-dark table-striped" style="width: 80%;">
         <Columns>   
             <asp:BoundField HeaderText="Descripcion" DataField="Descripcion"/>
             <asp:BoundField HeaderText="Precio" DataField="Precio"/>
         </Columns>
     </asp:GridView>
-    <asp:Label ID="lblTotal" runat="server" Text="Total: " style="font-size: 30px; color: white;"></asp:Label>
-    <asp:TextBox ID="txtTotal" runat="server" style="margin-left: 10px;" ></asp:TextBox>
+    <asp:Label ID="lblTotal" runat="server" Text="Total $: " style="font-size: 30px; color: white;"></asp:Label>
+    <asp:TextBox ID="txtTotal" runat="server" style="margin-left: 10px;width: 100px;" ></asp:TextBox>
 
 </asp:Content>
--- RESTO/Reporte.aspx.cs
+++ RESTO/Reporte.aspx.cs
@@ -1,9 +1,6 @@
 ﻿using Dominio;
 using Manager;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,40 +8,78 @@ namespace RESTO
 {
     public partial class Reporte : System.Web.UI.Page
     {
-
-        ServicioPedido servicioPedido = new ServicioPedido(); 
+        ServicioPedido servicioPedido = new ServicioPedido();
         ServicioUsuario servicioUsuario = new ServicioUsuario();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
+                
+                var listaIdMeseros = servicioUsuario.ListarIdMeseros();
 
-                ddlReporte.DataSource = servicioUsuario.ListarIdMeseros();
+                ddlReporte.DataSource = listaIdMeseros;
                 ddlReporte.DataValueField = "IdUsuario";
+                ddlReporte.DataTextField = "IdUsuario"; 
                 ddlReporte.DataBind();
+                ddlReporte.Items.Insert(0, new ListItem("Seleccione un mesero", "0")); 
 
-                ddlReporte.SelectedIndex = 0;
-
-                try
-                {
-                    int IdUsuario = Convert.ToInt32(ddlReporte.SelectedItem.Value);
-                    dgvReporte.DataSource = servicioPedido.ListaReportePorMesero(IdUsuario);
-                    dgvReporte.DataBind();
-                }
-                catch (Exception)
-                {
-                    Response.Write("<script>alert('No se seleccionó Mesero para eliminar.')</script>");
-                }
-
-
+                
+                MostrarPedidos();
             }
-
-
         }
+
 
         protected void ddlReporte_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            MostrarPedidos();
+            CalcularTotal();
         }
+
+        private void MostrarPedidos()
+        {
+            try
+            {
+                int IdUsuario = Convert.ToInt32(ddlReporte.SelectedValue);
+
+                if (IdUsuario != 0) 
+                {
+                    dgvReporte.DataSource = servicioPedido.ListaReportePorMesero(IdUsuario);
+                    dgvReporte.DataBind();
+                }
+                else
+                {
+                    
+                    dgvReporte.DataSource = null;
+                    dgvReporte.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Response.Write("<script>alert('Error al mostrar los pedidos: " + ex.Message + "')</script>");
+            }
+        }
+
+        private void CalcularTotal()
+        {
+            decimal total = 0;
+
+            foreach (GridViewRow row in dgvReporte.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    decimal precio;
+                    if (decimal.TryParse(row.Cells[1].Text, out precio)) 
+                    {
+                        total += precio;
+                    }
+                }
+            }
+
+            
+            txtTotal.Text = total.ToString();
+        }
+
     }
 }
